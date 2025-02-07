4c1ac204afdec1bbc35fe21ac6495e09d4b513eb
Fish <fishagbalo@gmail.com>
2025-02-07 23:40:30 +0800

    Remove unused chart components and clean up LineChart imports

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,4 +1,4 @@
-import React, { use, useEffect } from "react";
+import React, { useEffect } from "react";
 import * as echarts from "echarts";
 import { postData } from "../data/chartData";
 import { commentData } from "../data/chartData";
